2c41449ca67482290cc404d2c405bdee71d0c7bf
ankit0pokhriyal <62211049+ankit0pokhriyal@users.noreply.github.com>
2023-08-18 00:51:17 +0530

    Update Lab_P1.c

--- Lab_P1.c
+++ Lab_P1.c
@@ -6,21 +6,15 @@
 void NextGreatest(int number[], int n) {
     int i = n - 1;
 	
-	// Start from the rightmost digit and find the first
-	// digit that is smaller than the digit next to it.
     while (i > 0 && number[i] <= number[i - 1]) {
         i--;
     }
 	
-	// If no such digit found, then all numbers are in
-	// descending order, and no greater number is possible.
-    if (i == 0 && number[i] <= number[i - 1]) {
+    if (i == 0 ) {
         printf("Next number not possible\n");
         return;
     }
 	
-	// Find the smallest digit on the right side of
-	// (i-1)'th digit that is greater than number[i-1].
     int x = number[i - 1];
     int smallest = i;
     for (int j = i + 1; j < n; j++) {
@@ -29,18 +23,16 @@ void NextGreatest(int number[], int n) {
         }
     }
 	
-	// Swap the above found smallest digit with (i-1)'th.
     int temp = number[smallest];
     number[smallest] = number[i - 1];
     number[i - 1] = temp;
 	
-	// Convert the list up to i-1 into a number.
     int result = 0;
     for (int j = 0; j < i; j++) {
         result = result * 10 + number[j];
     }
 	
-	// Sort the digits after i-1 in ascending order.
+	
     for (int j = i; j < n - 1; j++) {
         for (int k = i; k < n - 1 - j + i; k++) {
             if (number[k] > number[k + 1]) {
@@ -51,7 +43,6 @@ void NextGreatest(int number[], int n) {
         }
     }
 	
-	// Convert the remaining sorted digits into a number.
     for (int j = i; j < n; j++) {
         result = result * 10 + number[j];
     }
