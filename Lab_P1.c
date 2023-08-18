214814c72a99c58d7438510fbf19eb507dcb3232
ankit0pokhriyal <62211049+ankit0pokhriyal@users.noreply.github.com>
2023-08-18 10:02:38 +0530

    Update Lab_P1.c

--- Lab_P1.c
+++ Lab_P1.c
@@ -5,11 +5,11 @@
 
 void NextGreatest(int number[], int n) {
     int i = n - 1;
-	
+    // condition for Descending order	
     while (i > 0 && number[i] <= number[i - 1]) {
         i--;
     }
-	
+    // Number already in Descending order
     if (i == 0 ) {
         printf("Next number not possible\n");
         return;
@@ -17,12 +17,13 @@ void NextGreatest(int number[], int n) {
 	
     int x = number[i - 1];
     int smallest = i;
+    // Look for smaller number
     for (int j = i + 1; j < n; j++) {
         if (number[j] > x && number[j] < number[smallest]) {
             smallest = j;
         }
     }
-	
+    // Keeping smaller number right side	
     int temp = number[smallest];
     number[smallest] = number[i - 1];
     number[i - 1] = temp;
@@ -32,7 +33,7 @@ void NextGreatest(int number[], int n) {
         result = result * 10 + number[j];
     }
 	
-	
+    // Swapping the Digit
     for (int j = i; j < n - 1; j++) {
         for (int k = i; k < n - 1 - j + i; k++) {
             if (number[k] > number[k + 1]) {
@@ -42,7 +43,7 @@ void NextGreatest(int number[], int n) {
             }
         }
     }
-	
+    // Finally form the resultant number	
     for (int j = i; j < n; j++) {
         result = result * 10 + number[j];
     }
